32c5e4c66d53ac667d968dc05f7c5d573cb94cfa
Iain Buclaw <iain.buclaw@sociomantic.com>
2016-07-18 21:22:59 +0200

    Convert to C, clean-up paths that won't be used

--- README.md
+++ README.md
@@ -1,22 +1,118 @@
 lua-rados
 =========
 
-<!--[![Build Status](https://travis-ci.org/noahdesu/lua-rados.png)](https://travis-ci.org/noahdesu/lua-rados)-->
-
 lua-rados is Lua bindings for the RADOS object store client library.
+
+This project is a fork of [lua-rados], but with all features removed apart
+from those to allow interaction from a read-only client.
 
 Please report bugs and make feature requests using the github tracker.
 
-Installation
-------------
+Example
+-------
 
-Install the latest version (v0.0.1) with [LuaRocks]:
+### NGINX config - Simple.
+```
+init_worker_by_lua_block
+{
+  cluster = rados.create()
+  cluster:conf_read_file()
+  cluster:connect()
+  ioctx = cluster:open_ioctx("rbd")
+}
 
-    luarocks install https://raw.github.com/noahdesu/lua-rados/master/rockspecs/lua-rados-0.0.1-1.rockspec
+server
+{
+  location /rbd
+  {
+    content_by_lua_block
+    {
+      local key = string.match(ngx.var.uri, "/rbd/(.+)")
+      if ioctx and key then
+        local size = ioctx:stat(key)
+          if size then
+            print(ioctx:read(key, size, 0))
+          end
+        end
+      }
+  }
+}
+```
 
-Documentation
--------------
+### NGINX config - Complex.
+```
+init_worker_by_lua_block
+{
+  -- Create a persistent connection to the object store
+  cluster = rados.create()
+  cluster:conf_read_file()
+  cluster:connect()
+  ioctx = cluster:open_ioctx("rbd")
+}
 
-Library documentation can be found at [http://noahdesu.github.io/lua-rados/](http://noahdesu.github.io/lua-rados/)
+server
+{
+  location /rbd
+  {
+    content_by_lua_block
+    {
+      local key = string.match(ngx.var.uri, "/rbd/(.+)")
+      -- We are connected and have matched a key
+      if ioctx and key then
+        local size, mtime = ioctx:stat(key)
+        -- Key exists in object store
+        if size and mtime then
+          -- We have an object to send, check client headers
+          local ims = ngx.var.http_if_modified_since
+          local range = ngx.var.http_range
+          local data = nil
 
-[LuaRocks]: http://www.luarocksporg "Lua package manager"
+          -- Set response headers
+          local hmtime = ngx.http_time(tonumber(mtime))
+          ngx.header["Last-Modified"] = hmtime
+
+          if ims == hmtime then
+            -- Not modified since last access
+            ngx.status = 304
+            ngx.send_headers()
+          else
+            local from, to = string.match(range or "", "bytes=([0-9]+)-([0-9]+)")
+            -- Send all or part of the object to the client
+            if from and to then
+              local offset = tonumber(from)
+              local rsize = tonumber(to) - offset + 1
+              if rsize < 1 then
+                -- Requested range not satisfiable
+                ngx.header["Content-Range"] = "bytes */" .. size
+                ngx.status = 416
+              else
+                data = ioctx:read(key, offset, rsize)
+                -- Return partial content to client
+                if data then
+                  ngx.header["Content-Range"] = "bytes " .. from .. "-" .. to .. "/" .. size
+                  ngx.status = 206
+                  size = rsize
+                end
+              end
+            else
+              data = ioctx:read(key, size, 0)
+            end
+
+            -- Fetched data without incident
+            if data then
+              ngx.header["Content-Length"] = size
+              ngx.print(data)
+            end
+          end
+        end
+      end
+      if ngx.status == 0 then
+        ngx.status = 404
+      end
+      ngx.exit(ngx.status)
+    }
+  }
+}
+```
+
+[lua-rados]: https://github.com/noahdesu/lua-rados "noahdesu/lua-rados"
---
+++ lua_rados.c
@@ -0,0 +1,486 @@
+/**
+  @module lua-rados
+*/
+#include <errno.h>
+#include <stdlib.h>
+
+#include <lua.h>
+#include <lualib.h>
+#include <lauxlib.h>
+
+#include <rados/librados.h>
+
+#define LRAD_TRADOS_T "Rados.RadosT"
+#define LRAD_TIOCTX_T "Rados.IoctxT"
+#define LRAD_BUFFER_T "Rados.Buffer"
+
+static char reg_key_rados_refs;
+
+typedef enum
+{
+  CREATED,
+  CONNECTED,
+  SHUTDOWN
+} cluster_state_t;
+
+typedef struct lua_rados_t
+{
+  rados_t cluster;
+  cluster_state_t state;
+} lua_rados_t;
+
+/* Get rados object at stack index.  */
+
+static inline lua_rados_t *
+lua_rados_checkcluster_1 (lua_State *lstate, int pos)
+{
+  return (lua_rados_t *) luaL_checkudata (lstate, pos, LRAD_TRADOS_T);
+}
+
+/* Get non-shutdown rados object at stack index. Error if had shutdown.  */
+
+static inline lua_rados_t *
+lua_rados_checkcluster (lua_State *lstate, int pos)
+{
+  lua_rados_t *rados;
+
+  rados = lua_rados_checkcluster_1 (lstate, pos);
+  if (rados->state == SHUTDOWN)
+    luaL_argerror (lstate, pos, "cannot reuse shutdown rados handle");
+
+  return rados;
+}
+
+/* Get connected rados object at stack index. Error if not connected.  */
+
+static inline lua_rados_t *
+lua_rados_checkcluster_conn (lua_State *lstate, int pos)
+{
+  lua_rados_t *rados;
+
+  rados = lua_rados_checkcluster (lstate, pos);
+  if (rados->state != CONNECTED)
+    luaL_argerror (lstate, pos, "not connected to cluster");
+
+  return rados;
+}
+
+/* Get rados_ioctx_t object.  */
+
+static inline rados_ioctx_t
+lua_rados_checkioctx (lua_State *lstate, int pos)
+{
+  return *(rados_ioctx_t *) luaL_checkudata (lstate, pos, LRAD_TIOCTX_T);
+}
+
+/* Push nil-error protocol values.  */
+
+static int
+lua_rados_pusherror (lua_State *lstate, int ret)
+{
+  lua_pushnil (lstate);
+  lua_pushfstring (lstate, "%s", strerror (-ret));
+  lua_pushinteger (lstate, ret);
+
+  return 3;
+}
+
+/* Push return value or error if not ok.  */
+
+static int
+lua_rados_pushresult (lua_State *lstate, int ok, int ret)
+{
+  if (!ok)
+    return lua_rados_pusherror (lstate, ret);
+
+  lua_pushinteger (lstate, ret);
+
+  return 1;
+}
+
+/* Garbage collect buffers in the heap.  */
+
+static int
+lua_rados_buffer_gc (lua_State *lstate)
+{
+  char **pbuf;
+
+  pbuf = (char **) luaL_checkudata (lstate, 1, LRAD_BUFFER_T);
+  if (pbuf && *pbuf)
+    {
+      free (*pbuf);
+      *pbuf = NULL;
+    }
+
+  return 0;
+}
+
+/* Allocate a new char * in the heap.  */
+
+static char *
+lua_rados_newbuffer (lua_State *lstate, int size)
+{
+  char *buf;
+  char **pbuf;
+
+  buf = (char *) malloc (size);
+  memset (buf, 0, size);
+
+  pbuf = (char **) lua_newuserdata (lstate, sizeof (*pbuf));
+  *pbuf = buf;
+
+  luaL_getmetatable (lstate, LRAD_BUFFER_T);
+  lua_setmetatable (lstate, -2);
+
+  return buf;
+}
+
+/**
+  @type Rados
+ */
+
+/**
+  Get the version of librados.
+  @function version
+  @return major version
+  @return minor version
+  @return extra version
+  @usage major, minor, extra = rados.version()
+ */
+static int
+lua_rados_version (lua_State *lstate)
+{
+  int major, minor, extra;
+
+  rados_version (&major, &minor, &extra);
+
+  lua_pushinteger (lstate, major);
+  lua_pushinteger (lstate, minor);
+  lua_pushinteger (lstate, extra);
+
+  return 3;
+}
+
+/**
+  Create handle for communicating with RADOS cluster.
+  @function create
+  @string id the user to connect as (optional, or nil)
+  @return rados handle object on success, nil otherwise
+  @return error message and retval if failed
+  @usage cluster = rados.create()
+  @usage cluster = rados.create('admin')
+*/
+static int
+lua_rados_create (lua_State *lstate)
+{
+  lua_rados_t *rados;
+  const char *id;
+  int ret;
+
+  if (lua_gettop (lstate) > 0 && lua_type (lstate, 1) != LUA_TNIL)
+    {
+      luaL_checktype (lstate, 1, LUA_TSTRING);
+      id = lua_tostring (lstate, 1);
+    }
+  else
+    id = NULL;
+
+  rados = (lua_rados_t *) lua_newuserdata (lstate, sizeof (*rados));
+  rados->state = CREATED;
+
+  luaL_getmetatable (lstate, LRAD_TRADOS_T);
+  lua_setmetatable (lstate, -2);
+
+  ret = rados_create (&rados->cluster, id);
+  if (ret)
+    return lua_rados_pusherror (lstate, ret);
+
+  /* return the userdata */
+  return 1;
+}
+
+/**
+  @type Cluster
+ */
+
+/**
+  Configure the rados handle using a Ceph config file.
+  @function conf_read_file
+  @string file path to configuration file (optional, or nil)
+  @return 0 on success, nil otherwise
+  @return error message and retval if failed
+  @usage cluster:conf_read_file()
+  @usage cluster:conf_read_file('/path/to/ceph.conf')
+ */
+static int
+lua_rados_conf_read_file (lua_State *lstate)
+{
+  lua_rados_t *rados;
+  const char *path;
+  int ret;
+
+  rados = lua_rados_checkcluster (lstate, 1);
+
+  if (lua_gettop (lstate) > 1 && lua_type (lstate, 2) != LUA_TNIL)
+    {
+      luaL_checktype (lstate, 2, LUA_TSTRING);
+      path = lua_tostring (lstate, 2);
+    }
+  else
+    path = NULL;
+
+  ret = rados_conf_read_file (rados->cluster, path);
+
+  return lua_rados_pushresult (lstate, (ret == 0), ret);
+}
+
+/**
+  Connect to the cluster.
+  @function connect
+  @return 0 on success, nil otherwise
+  @return error message and retval if failed
+  @usage cluster:connect()
+  @usage status, errstr, ret = cluster:connect()
+ */
+static int
+lua_rados_connect (lua_State *lstate)
+{
+  lua_rados_t *rados;
+  int ret;
+
+  rados = lua_rados_checkcluster (lstate, 1);
+  if (rados->state == CONNECTED)
+    luaL_argerror (lstate, 1, "already connected to cluster");
+
+  ret = rados_connect (rados->cluster);
+  if (!ret)
+    rados->state = CONNECTED;
+
+  return lua_rados_pushresult (lstate, (ret == 0), ret);
+}
+
+/**
+  Disconnect from the cluster.
+  @function shutdown
+  @usage cluster:shutdown()
+ */
+static int
+lua_rados_shutdown (lua_State *lstate)
+{
+  lua_rados_t *rados;
+
+  rados = lua_rados_checkcluster_conn (lstate, 1);
+  if (rados->state != SHUTDOWN)
+    rados_shutdown (rados->cluster);
+
+  rados->state = SHUTDOWN;
+
+  return 0;
+}
+
+/**
+  Create an I/O context.
+  @function open_ioctx
+  @string name of pool
+  @return I/O context object or nil on failure
+  @return errstr, ret if failed
+  @usage ioctx = cluster:open_ioctx('my_pool')
+ */
+static int
+lua_rados_open_ioctx (lua_State *lstate)
+{
+  lua_rados_t *rados;
+  const char *pool_name;
+  rados_ioctx_t *ioctx;
+  int ret;
+
+  rados = lua_rados_checkcluster_conn (lstate, 1);
+  pool_name = luaL_checkstring (lstate, 2);
+  ioctx = (rados_ioctx_t *) lua_newuserdata (lstate, sizeof (*ioctx));
+  luaL_getmetatable (lstate, LRAD_TIOCTX_T);
+  lua_setmetatable (lstate, -2);
+
+  ret = rados_ioctx_create (rados->cluster, pool_name, ioctx);
+  if (ret)
+    return lua_rados_pusherror (lstate, ret);
+
+  /* record rados_ioctx_t -> rados_t reference in weak key table */
+  lua_pushlightuserdata (lstate, &reg_key_rados_refs);
+  lua_gettable (lstate, LUA_REGISTRYINDEX);
+
+  if (lua_isnil (lstate, -1) || lua_type (lstate, -1) != LUA_TTABLE)
+    return lua_rados_pusherror (lstate, 1);
+
+  lua_pushvalue (lstate, -2); /* key = ioctx */
+  lua_pushvalue (lstate, 1);  /* value = cluster */
+  lua_settable (lstate, -3);
+  lua_pop (lstate, 1);
+
+  /* return the userdata */
+  return 1;
+}
+
+/* Garbage collect the rados object, and shutdown only if connected.  */
+
+static int
+lua_rados_cluster_gc (lua_State *lstate)
+{
+  lua_rados_t *rados;
+
+  rados = lua_rados_checkcluster_1 (lstate, 1);
+
+  if (rados->state == CONNECTED)
+    rados_shutdown (rados->cluster);
+
+  rados->state = SHUTDOWN;
+
+  return 0;
+}
+
+/**
+  @type Ioctx
+ */
+
+/**
+  Close the I/O context.
+  @function close
+  @usage ioctx:close()
+ */
+static int
+lua_rados_ioctx_close (lua_State *lstate)
+{
+  rados_ioctx_t io;
+
+  io = lua_rados_checkioctx (lstate, 1);
+  rados_ioctx_destroy (io);
+
+  return 0;
+}
+
+/**
+  Get object stat info (size/mtime)
+  @function stat
+  @string oid object name
+  @return len, mtime, or nil on failure
+  @return errstr and retval if failed
+  @usage size, mtime = ioctx:stat('obj3')
+ */
+static int
+lua_rados_ioctx_stat (lua_State *lstate)
+{
+  rados_ioctx_t io;
+  const char *oid;
+  uint64_t size;
+  time_t mtime;
+  int ret;
+
+  io = lua_rados_checkioctx (lstate, 1);
+  oid = luaL_checkstring (lstate, 2);
+
+  ret = rados_stat (io, oid, &size, &mtime);
+  if (ret)
+    return lua_rados_pusherror (lstate, ret);
+
+  lua_pushinteger (lstate, size);
+  lua_pushinteger (lstate, mtime);
+
+  return 2;
+}
+
+/**
+  Read data from an object.
+  @function read
+  @string oid object name
+  @int length number of bytes read
+  @int offset offset in object from which to read
+  @return string with at most `length` bytes, or nil on error
+  @return errstr and retval if failed
+  @usage data = ioctx:read('obj3', 1000, 0)
+ */
+static int
+lua_rados_ioctx_read (lua_State *lstate)
+{
+  rados_ioctx_t io;
+  const char *oid;
+  size_t size;
+  uint64_t off;
+  char *buf;
+  int ret;
+
+  io = lua_rados_checkioctx (lstate, 1);
+  oid = luaL_checkstring (lstate, 2);
+  size = luaL_checkint (lstate, 3);
+  off = luaL_checkint (lstate, 4);
+  buf = lua_rados_newbuffer (lstate, size);
+
+  ret = rados_read (io, oid, buf, size, off);
+  if (ret < 0)
+    return lua_rados_pusherror (lstate, ret);
+
+  lua_pushlstring (lstate, buf, ret);
+
+  return 1;
+}
+
+
+static const luaL_Reg clusterlib_m[] =
+{
+  { "conf_read_file", lua_rados_conf_read_file },
+  { "connect", lua_rados_connect },
+  { "shutdown", lua_rados_shutdown },
+  { "open_ioctx", lua_rados_open_ioctx },
+  { "__gc", lua_rados_cluster_gc },
+  { NULL, NULL }
+};
+
+static const luaL_Reg ioctxlib_m[] =
+{
+  { "close", lua_rados_ioctx_close},
+  { "read", lua_rados_ioctx_read},
+  { "stat", lua_rados_ioctx_stat},
+  { NULL, NULL }
+};
+
+static const luaL_Reg radoslib_f[] =
+{
+  { "version", lua_rados_version },
+  { "create", lua_rados_create },
+  { NULL, NULL }
+};
+
+LUALIB_API int
+luaopen_rados (lua_State *lstate)
+{
+  /* setup rados_t userdata type */
+  luaL_newmetatable (lstate, LRAD_TRADOS_T);
+  lua_pushvalue (lstate, -1);
+  lua_setfield (lstate, -2, "__index");
+  luaL_register (lstate, NULL, clusterlib_m);
+  lua_pop (lstate, 1);
+
+  /* setup rados_ioctx_t userdata type */
+  luaL_newmetatable (lstate, LRAD_TIOCTX_T);
+  lua_pushvalue (lstate, -1);
+  lua_setfield (lstate, -2, "__index");
+  luaL_register (lstate, NULL, ioctxlib_m);
+  lua_pop (lstate, 1);
+
+  /* setup buffer userdata type */
+  luaL_newmetatable (lstate, LRAD_BUFFER_T);
+  lua_pushcfunction (lstate, lua_rados_buffer_gc);
+  lua_setfield (lstate, -2, "__gc");
+  lua_pop (lstate, 1);
+
+  /* weak table to protect rados_ioctx_t -> rados_t refs */
+  lua_pushlightuserdata (lstate, &reg_key_rados_refs);
+  lua_newtable (lstate);
+  lua_pushstring (lstate, "k");
+  lua_setfield (lstate, -2, "__mode");
+  lua_pushvalue (lstate, -1);
+  lua_setmetatable (lstate, -2);
+  lua_settable (lstate, LUA_REGISTRYINDEX);
+
+  luaL_register (lstate, "rados", radoslib_f);
+
+  return 0;
+}
